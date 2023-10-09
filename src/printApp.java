f8c98ec1f22bc7287fd2457b1dbea404bbdd1010
Kapukankanamage Don Supun Wathsana Weerasinghe <spn.weer@gmail.com>
2023-10-09 15:09:42 +0300

    Added Comment and Declare variables for numbers and results

--- src/printApp.java
+++ src/printApp.java
@@ -4,6 +4,23 @@ public class printApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// Declare variables for numbers and results
+		int Number1;
+		int Number2;
+		int multiplication;
+		int addition;
+		double division;
+		int Substraction;
+		String creater;
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
